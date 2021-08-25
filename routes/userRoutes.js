795f8b56f3cc7abb5c58eb9d72936a9ca9ed0ffb
JaneStack <mrumunjanet@gmail.com>
2021-08-25 13:10:45 +0100

    added missing authentication, and authorization

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -17,22 +17,25 @@ router.post('/login', authController.login);
 router.post('/forgotPassword', authController.forgotPassword);
 router.patch('/resetPassword/:token', authController.resetPassword);
 
+//protect all routes after this middleware
+router.use(authController.protect);
+
 router.patch(
     '/updateMyPassword',
-    authController.protect,
     authController.updatePassword
 
 );
 
 router.get(
     '/me',
-    authController.protect,
     userController.getMe,
     userController.getUser
 );
 
-router.patch('/updateMe', authController.protect, userController.updateMe)
-router.delete('/deleteMe', authController.protect, userController.deleteme)
+router.patch('/updateMe', userController.updateMe)
+router.delete('/deleteMe', userController.deleteme)
+
+router.use(authController.restrictTo('admin'))
 
 router
     .route('/')
